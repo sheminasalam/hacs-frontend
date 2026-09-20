3d681c5e5fbeb1766a2e84e0ecdf475a0ea8dd75
Shemin A Salam <45196177+sheminasalam@users.noreply.github.com>
2026-09-20 10:58:01 +0530

    Update websocket.ts

--- src/data/websocket.ts
+++ src/data/websocket.ts
@@ -3,6 +3,18 @@ import type { Hacs, HacsInfo } from "./hacs";
 import type { HacsDispatchEvent } from "./common";
 import type { RepositoryBase } from "./repository";
 
+export interface HacsListRepository {
+  id: string;
+  full_name: string;
+}
+
+export interface HacsList {
+  id: string;
+  name: string;
+  builtin: boolean;
+  repositories: HacsListRepository[];
+}
+
 export const fetchHacsInfo = async (hass: HomeAssistant) =>
   hass.connection.sendMessagePromise<HacsInfo>({
     type: "hacs/info",
@@ -11,6 +23,41 @@ export const fetchHacsInfo = async (hass: HomeAssistant) =>
 export const getRepositories = async (hass: HomeAssistant) =>
   hass.connection.sendMessagePromise<RepositoryBase[]>({
     type: "hacs/repositories/list",
+  });
+
+export const getLists = async (hass: HomeAssistant) =>
+  hass.connection.sendMessagePromise<HacsList[]>({
+    type: "hacs/lists/list",
+  });
+
+export const createList = async (hass: HomeAssistant, name: string) =>
+  hass.connection.sendMessagePromise<HacsList[]>({
+    type: "hacs/lists/create",
+    name,
+  });
+
+export const renameList = async (hass: HomeAssistant, listId: string, name: string) =>
+  hass.connection.sendMessagePromise<HacsList[]>({
+    type: "hacs/lists/rename",
+    list_id: listId,
+    name,
+  });
+
+export const deleteList = async (hass: HomeAssistant, listId: string) =>
+  hass.connection.sendMessagePromise<HacsList[]>({
+    type: "hacs/lists/delete",
+    list_id: listId,
+  });
+
+export const setRepositoryLists = async (
+  hass: HomeAssistant,
+  repository: string,
+  lists: string[],
+) =>
+  hass.connection.sendMessagePromise<HacsList[]>({
+    type: "hacs/lists/set_repository",
+    repository,
+    lists,
   });
 
 export const repositoryUninstall = async (hass: HomeAssistant, repository: string) =>
